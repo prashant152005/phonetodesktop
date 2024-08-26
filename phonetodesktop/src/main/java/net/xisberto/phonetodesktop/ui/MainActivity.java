6b1b41cb7228f4e5952158565a8966fe3f82f3fa
Prashant K Pathak <123973637+prashant152005@users.noreply.github.com>
2024-08-26 22:18:31 +0530

    Update MainActivity.java

--- phonetodesktop/src/main/java/net/xisberto/phonetodesktop/ui/MainActivity.java
+++ phonetodesktop/src/main/java/net/xisberto/phonetodesktop/ui/MainActivity.java
@@ -18,15 +18,14 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.StringRes;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
+import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.google.android.gms.auth.UserRecoverableAuthException;
 import com.google.android.gms.common.AccountPicker;
@@ -41,24 +40,20 @@ import net.xisberto.phonetodesktop.Utils;
 import net.xisberto.phonetodesktop.network.GoogleTasksSpiceService;
 import net.xisberto.phonetodesktop.network.TasksListRequest;
 
-public class MainActivity extends AppCompatActivity implements
-        MainFragment.PhoneToDesktopAuthorization {
+public class MainActivity extends AppCompatActivity implements MainFragment.PhoneToDesktopAuthorization {
 
-    public static final int REQUEST_GOOGLE_PLAY_SERVICES = 0;
-    public static final int REQUEST_AUTHORIZATION = 1;
-    public static final int REQUEST_ACCOUNT_PICKER = 2;
-    private static final int REQUEST_PERMISSION = 3;
+    private static final int REQUEST_GOOGLE_PLAY_SERVICES = 0;
+    private static final int REQUEST_AUTHORIZATION = 1;
+    private static final int REQUEST_ACCOUNT_PICKER = 2;
 
     private static final String TAG_MAIN = "mainFragment";
-    public Preferences preferences;
+    
+    private Preferences preferences;
     private MainFragment mainFragment;
-    private Fragment currentFragment;
-
     private boolean showWelcome;
+    private final SpiceManager spiceManager = new SpiceManager(GoogleTasksSpiceService.class);
 
-    private SpiceManager mSpiceManager = new SpiceManager(GoogleTasksSpiceService.class);
-
-    private BroadcastReceiver receiver = new BroadcastReceiver() {
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (intent.hasExtra(Utils.EXTRA_UPDATING)) {
@@ -68,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements
     };
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_main);
@@ -76,22 +71,9 @@ public class MainActivity extends AppCompatActivity implements
         preferences = Preferences.getInstance(this);
 
         if (savedInstanceState == null) {
-            FragmentTransaction transaction = getSupportFragmentManager()
-                    .beginTransaction();
-
-            if (preferences.loadAccountName() == null) {
-                showWelcome = true;
-                currentFragment = WelcomeFragment.newInstance();
-                transaction.replace(R.id.main_frame, currentFragment);
-            } else {
-                showWelcome = false;
-                mainFragment = MainFragment.newInstance();
-                transaction.replace(R.id.main_frame, mainFragment, TAG_MAIN);
-            }
-            transaction.commit();
+            initFragment();
         } else {
-            mainFragment = (MainFragment) getSupportFragmentManager().findFragmentByTag(
-                    TAG_MAIN);
+            mainFragment = (MainFragment) getSupportFragmentManager().findFragmentByTag(TAG_MAIN);
         }
 
         checkActionAuth(getIntent());
@@ -104,45 +86,25 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     private void checkActionAuth(Intent intent) {
-        String action = intent.getAction();
-        if (action != null && action.equals(Utils.ACTION_AUTHENTICATE)) {
+        if (Utils.ACTION_AUTHENTICATE.equals(intent.getAction())) {
             startAuthorization();
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         switch (requestCode) {
             case REQUEST_ACCOUNT_PICKER:
-                Utils.log("Result from Account Picker");
-                if (resultCode == RESULT_OK && data != null
-                        && data.hasExtra(AccountManager.KEY_ACCOUNT_NAME)) {
-                    String accountName = data.getExtras().getString(
-                            AccountManager.KEY_ACCOUNT_NAME);
-                    Utils.log("Saving account " + accountName);
-                    preferences.saveAccountName(accountName);
-                    saveListId();
-                    updateMainLayout(true);
-                } else {
-                    updateMainLayout(false);
-                }
+                handleAccountPickerResult(resultCode, data);
                 break;
-
             case REQUEST_GOOGLE_PLAY_SERVICES:
-                Utils.log("Return from Play Services error");
+                handleGooglePlayServicesResult(resultCode);
                 break;
             case REQUEST_AUTHORIZATION:
-                Utils.log("Result from Authorization");
-                if (resultCode == RESULT_OK) {
-                    Utils.log("starting saveListId");
-                    updateMainLayout(true);
-                    saveListId();
-                } else {
-                    updateMainLayout(false);
-                }
+                handleAuthorizationResult(resultCode);
                 break;
             default:
+                super.onActivityResult(requestCode, resultCode, data);
                 break;
         }
     }
@@ -150,20 +112,10 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     protected void onStart() {
         super.onStart();
-        LocalBroadcastManager.getInstance(this).registerReceiver(receiver,
-                new IntentFilter(Utils.ACTION_AUTHENTICATE));
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, new IntentFilter(Utils.ACTION_AUTHENTICATE));
 
         if (!showWelcome && currentFragment instanceof WelcomeFragment) {
-            mainFragment = (MainFragment) getSupportFragmentManager()
-                    .findFragmentByTag(TAG_MAIN);
-            if (mainFragment == null) {
-                mainFragment = MainFragment.newInstance();
-            }
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.main_frame, mainFragment)
-                    .setTransition(FragmentTransaction.TRANSIT_NONE)
-                    .commit();
-            updateMainLayout(false);
+            switchToMainFragment();
         }
     }
 
@@ -171,39 +123,31 @@ public class MainActivity extends AppCompatActivity implements
     protected void onStop() {
         super.onStop();
         LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
-        if (mSpiceManager.isStarted()) {
-            mSpiceManager.shouldStop();
+        if (spiceManager.isStarted()) {
+            spiceManager.shouldStop();
         }
     }
 
     @Override
     public void startAuthorization() {
-        if (checkGooglePlayServicesAvailable()) {
-            // ask user to choose account
+        if (isGooglePlayServicesAvailable()) {
             updateMainLayout(true);
             preferences.saveListId(null);
-            Intent accountPicker = AccountPicker.newChooseAccountIntent(null, null,
-                    new String[]{"com.google"}, true, null, null, null, null);
-            startActivityForResult(accountPicker, REQUEST_ACCOUNT_PICKER);
+            startActivityForResult(AccountPicker.newChooseAccountIntent(null, null, new String[]{"com.google"}, true, null, null, null, null), REQUEST_ACCOUNT_PICKER);
         }
     }
 
-    /**
-     * Check that Google Play services APK is installed and up to date.
-     */
-    public boolean checkGooglePlayServicesAvailable() {
+    private boolean isGooglePlayServicesAvailable() {
         GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
-        int connectionStatusCode = googleApiAvailability.isGooglePlayServicesAvailable(this);
-        if (googleApiAvailability.isUserResolvableError(connectionStatusCode)) {
-            googleApiAvailability.showErrorDialogFragment(
-                    this, connectionStatusCode, REQUEST_GOOGLE_PLAY_SERVICES);
+        int statusCode = googleApiAvailability.isGooglePlayServicesAvailable(this);
+        if (googleApiAvailability.isUserResolvableError(statusCode)) {
+            googleApiAvailability.showErrorDialogFragment(this, statusCode, REQUEST_GOOGLE_PLAY_SERVICES);
             return false;
         }
         return true;
     }
 
     private void updateMainLayout(boolean updating) {
-        Utils.log("updating layout " + updating);
         if (mainFragment != null) {
             mainFragment.setUpdating(updating);
             if (mainFragment.isVisible()) {
@@ -212,76 +156,116 @@ public class MainActivity extends AppCompatActivity implements
         }
     }
 
-    /**
-     * Retrieves the list id to use. Creates a new list if no list named "PhoneToDesktop" is found.
-     */
     private void saveListId() {
-        if (!mSpiceManager.isStarted()) {
-            mSpiceManager.start(this);
+        if (!spiceManager.isStarted()) {
+            spiceManager.start(this);
         }
 
         RequestListener<Void> listRequestListener = new RequestListener<Void>() {
             @Override
             public void onRequestFailure(SpiceException spiceException) {
-                if (spiceException.getCause() instanceof UserRecoverableAuthException) {
-                    UserRecoverableAuthException userRecoverableAuthException =
-                            (UserRecoverableAuthException) spiceException.getCause();
-                    startActivityForResult(userRecoverableAuthException.getIntent(),
-                            MainActivity.REQUEST_AUTHORIZATION);
-                } else {
-                    RetryDialog dialog = RetryDialog.newInstance(R.string.txt_retry,
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    switch (which) {
-                                        case DialogInterface.BUTTON_POSITIVE:
-                                            saveListId();
-                                        case DialogInterface.BUTTON_NEGATIVE:
-                                            updateMainLayout(false);
-                                    }
-                                }
-                            });
-                    dialog.show(getSupportFragmentManager(), "retry_dialog");
-                }
+                handleSaveListIdFailure(spiceException);
             }
 
             @Override
             public void onRequestSuccess(Void aVoid) {
-                Utils.log("saveListId success");
-                updateMainLayout(false);
-                if (showWelcome) {
-                    startActivity(new Intent(MainActivity.this, TutorialActivity.class));
-                    showWelcome = false;
-                }
+                onSaveListIdSuccess();
             }
         };
 
         TasksListRequest request = new TasksListRequest(this);
-        mSpiceManager.execute(request, listRequestListener);
+        spiceManager.execute(request, listRequestListener);
+    }
+
+    private void initFragment() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        if (preferences.loadAccountName() == null) {
+            showWelcome = true;
+            transaction.replace(R.id.main_frame, WelcomeFragment.newInstance());
+        } else {
+            showWelcome = false;
+            mainFragment = MainFragment.newInstance();
+            transaction.replace(R.id.main_frame, mainFragment, TAG_MAIN);
+        }
+        transaction.commit();
+    }
+
+    private void handleAccountPickerResult(int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && data != null && data.hasExtra(AccountManager.KEY_ACCOUNT_NAME)) {
+            String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+            preferences.saveAccountName(accountName);
+            saveListId();
+            updateMainLayout(true);
+        } else {
+            updateMainLayout(false);
+        }
+    }
+
+    private void handleGooglePlayServicesResult(int resultCode) {
+        // Handle result from Google Play Services
+    }
+
+    private void handleAuthorizationResult(int resultCode) {
+        if (resultCode == RESULT_OK) {
+            updateMainLayout(true);
+            saveListId();
+        } else {
+            updateMainLayout(false);
+        }
+    }
+
+    private void handleSaveListIdFailure(SpiceException spiceException) {
+        if (spiceException.getCause() instanceof UserRecoverableAuthException) {
+            UserRecoverableAuthException authException = (UserRecoverableAuthException) spiceException.getCause();
+            startActivityForResult(authException.getIntent(), REQUEST_AUTHORIZATION);
+        } else {
+            showRetryDialog();
+        }
+    }
+
+    private void onSaveListIdSuccess() {
+        updateMainLayout(false);
+        if (showWelcome) {
+            startActivity(new Intent(MainActivity.this, TutorialActivity.class));
+            showWelcome = false;
+        }
+    }
+
+    private void showRetryDialog() {
+        RetryDialog dialog = RetryDialog.newInstance(R.string.txt_retry, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == DialogInterface.BUTTON_POSITIVE) {
+                    saveListId();
+                } else if (which == DialogInterface.BUTTON_NEGATIVE) {
+                    updateMainLayout(false);
+                }
+            }
+        });
+        dialog.show(getSupportFragmentManager(), "retry_dialog");
     }
 
     public static class RetryDialog extends DialogFragment {
 
-        private int mMessage;
-        private DialogInterface.OnClickListener mListener;
+        private int messageResId;
+        private DialogInterface.OnClickListener listener;
 
         public static RetryDialog newInstance(@StringRes int message, DialogInterface.OnClickListener listener) {
             RetryDialog dialog = new RetryDialog();
-            dialog.mMessage = message;
-            dialog.mListener = listener;
+            dialog.messageResId = message;
+            dialog.listener = listener;
             return dialog;
         }
 
         @NonNull
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
-            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext())
+            return new AlertDialog.Builder(requireContext())
                     .setTitle(R.string.app_name)
-                    .setMessage(mMessage)
-                    .setPositiveButton(R.string.retry, mListener)
-                    .setNegativeButton(android.R.string.cancel, mListener);
-            return dialogBuilder.create();
+                    .setMessage(messageResId)
+                    .setPositiveButton(R.string.retry, listener)
+                    .setNegativeButton(android.R.string.cancel, listener)
+                    .create();
         }
-
     }
 }
